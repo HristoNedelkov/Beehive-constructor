9f3f18a0958de2e1873b05c97235f58608cc27f8
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-12-20 09:43:20 +0200

    update gallery

---
+++ images/beehive-prototype/lastPrototype/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+// Directory of the current script
+const directoryPath = path.join(__dirname);
+
+// Read directory
+fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+
+    // Create an array to hold the file names
+    const fileList = [];
+
+    // Add each file to the array
+    files.forEach(file => {
+        fileList.push(file);
+    });
+
+    // Console log the array
+    console.log(fileList);
+});
