bc72166d12d515978dc7bda8a6557709ce0fc046
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2024-04-10 22:52:56 +0300

    Use emailjs for the form in the SITE

--- comments.js
+++ comments.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import emailjs from "https://cdn.skypack.dev/@emailjs/browser";
 import {
   getDatabase,
   set,
@@ -8,7 +9,7 @@ import {
   get,
 } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 const firebaseConfig = {
-  apiKey: "AIzaSyA-9hqU27gOOZTCM6VEuYtwEkbC6LI-sgo",
+  apiKey: "",// YOU NEED TO ADD THIS TO USE FIREBASE!!!
   authDomain: "jana-vector-and-graph.firebaseapp.com",
   databaseURL: "https://jana-vector-and-graph-default-rtdb.firebaseio.com",
   projectId: "jana-vector-and-graph",
@@ -17,26 +18,28 @@ const firebaseConfig = {
   appId: "1:6395513197:web:70450612dbe7ed1eabb264",
 };
 
-initializeApp(firebaseConfig);
-// Initialize Firebase
+ 
 
 const dbRef = getDatabase();
 
-const commentsRef = ref(dbRef, "comments");
+//const commentsRef = ref(dbRef, "comments");
 export function uploadComment({ name, email, message }) {
-  const newPostRef = push(commentsRef);
-  set(newPostRef, {
-    name,
-    email,
-    message,
-  })
-    .then((res) => {
-      alert("Comment posted successfully");
-      console.log(name + " said " + message);
-    })
-    .catch((e) => {
-      console.log("unseccessful, errror: " + e);
-    });
+  // const newPostRef = push(commentsRef);
+
+  const serviceID = "service_4dhm85f";
+  const templateID = "template_d3niw1s";
+  emailjs.init("QkQM3bAxeRcLBV9Af");
+
+  const form = document.querySelector("form");
+
+  emailjs.sendForm(serviceID, templateID, form).then(
+    () => {
+      alert("Благодаря, че се свързахте с нас! Ще се свържем с вас в най-кратък срок! :)");
+    },
+    (err) => {
+      alert(JSON.stringify(err));
+    }
+  );
 }
 
 export function getAllComments() {
